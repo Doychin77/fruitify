b1747912b48164e0f5cd23944f0ff3d96e7be939
Doychin Yanev <114282133+Doychin77@users.noreply.github.com>
2024-10-02 10:40:28 +0300

    upd

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CouponController;
+use App\Http\Controllers\EcontController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Models\BlogCategory;
@@ -18,6 +19,10 @@ Route::get('/articles', [BlogController::class, 'allArticles']);
 Route::get('/article/{id}', [BlogController::class, 'getArticle']);
 Route::post('/contact', [ContactController::class, 'sendContactForm']);
 Route::post('/orders', [OrderController::class, 'store']);
+
+Route::get('/econt/get_cities', [EcontController::class, 'getCities'])->name('econt.get.cities');
+Route::get('/econt/get_offices', [EcontController::class, 'getOffices'])->name('econt.get.offices');
+Route::get('/econt/get_streets', [EcontController::class, 'getStreets'])->name('econt.get.streets');
 
 
 Route::get('/', function () {

e99fdaa46ea92f8f28162b9ecfded15f964e16a6
Doychin Yanev <114282133+Doychin77@users.noreply.github.com>
2024-10-10 14:55:32 +0300

    upd

--- resources/js/src/components/Header.jsx
+++ resources/js/src/components/Header.jsx
@@ -110,7 +110,7 @@ const Header = () => {
                     <div className="col-lg-3">
                         <div className="header__logo">
                             <Link to="/">
-                                <img src="img/logo.png" alt="" />
+                                <img src="img/logo2.png" alt="" />
                             </Link>
                         </div>
                     </div>

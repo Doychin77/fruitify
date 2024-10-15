42fead6b7db7f9a9012ea64cb168003085690709
Doychin Yanev <114282133+Doychin77@users.noreply.github.com>
2024-10-15 14:57:32 +0300

    upd

--- app/Filament/Resources/BlogArticleResource.php
+++ app/Filament/Resources/BlogArticleResource.php
@@ -23,7 +23,7 @@ class BlogArticleResource extends Resource
 {
     protected static ?string $model = BlogArticle::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     protected static ?string $navigationGroup = 'Блог';
 
--- app/Filament/Resources/BlogCategoryResource.php
+++ app/Filament/Resources/BlogCategoryResource.php
@@ -18,7 +18,7 @@ class BlogCategoryResource extends Resource
 {
     protected static ?string $model = BlogCategory::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-square-3-stack-3d';
 
     protected static ?string $navigationGroup = 'Блог';
 

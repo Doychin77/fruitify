6a6bc89069cb8cc0b49bdc801219cc6d76e8a67a
Doychin Yanev <hdskhrystal@gmail.com>
2024-11-11 14:02:42 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
     return {
-        base: env.APP_URL,
+        base: `${env.APP_URL}/`,
         define: {
             "process.env.APP_URL": JSON.stringify(env.APP_URL),
         },

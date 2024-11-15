5db79d74f04383fb7551ade0ccdec85efd346252
Doychin Yanev <hdskhrystal@gmail.com>
2024-11-15 09:41:54 +0200

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
@@ -19,12 +20,14 @@ export default defineConfig(({ mode }) => {
                 ],
                 refresh: true,
             }),
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: 'public/fonts/*',
+                        dest: 'fonts',
+                    }
+                ]
+            }),
         ],
-
-        // build: {
-        //     // outDir: 'public/build',
-        //     manifest: true, // Include a manifest file
-        //     assetsDir: '', // Avoid nested directories for assets
-        // },
     };
 });
